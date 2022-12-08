f230533bdf5649fa0368cdab103ea22dc425be49
Jamiu Kolawole <Jakko4real@gmail.com>
2022-12-08 20:33:35 +0100

    Update axios.js
    chore: updating backend server link

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://api-bane-ecommerce.herokuapp.com" // THE API (backend) URL
+    baseURL: "https://devat-ecommerce-api.onrender.com" // THE API (backend) URL
 });
 
 export default instance;
